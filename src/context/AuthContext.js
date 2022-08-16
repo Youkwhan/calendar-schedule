7ea37c000775670a7bb1d6f725099ec6e4fca939
Youkwhan <37788922+Youkwhan@users.noreply.github.com>
2022-08-16 18:08:49 -0400

    google auth

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,9 +1,25 @@
 import { useContext, createContext } from "react";
+import {
+	GoogleAuthProvider,
+	signInWithPopup,
+	signInWithRedirect,
+	signOut,
+	onAuthStateChanged,
+} from "firebase/auth";
+import {auth} from '../firebase-config'
 
 const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
-	return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+	const googleSignIn = () => {
+		const provider = new GoogleAuthProvider();
+		signInWithPopup(auth, provider);
+	};
+	return (
+		<AuthContext.Provider value={{ googleSignIn }}>
+			{children}
+		</AuthContext.Provider>
+	);
 };
 
 export const UserAuth = () => {
