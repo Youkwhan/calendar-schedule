48a52a684fded2e8bd93b8cd59f93d5f576f496c
Youkwhan <37788922+Youkwhan@users.noreply.github.com>
2022-08-17 22:10:15 -0400

    test

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -14,12 +14,12 @@ const Account = () => {
 	};
 
 	return (
-		<div className="w-[300px] m-auto">
-			<h1 className="text-center text-2xl font-bold pt-12">Account</h1>
+		<div>
+			<h1 >Account</h1>
 			<div>
 				<p>Welcome, {user?.displayName}</p>
 			</div>
-			<button onClick={handleSignOut} className="border py-2 px-5 mt-10">
+			<button onClick={handleSignOut}>
 				Logout
 			</button>
 		</div>
